e9a9d48376fdeb8af9088570340bc6d4d24fed99
MGStigger <stigger1999@gmail.com>
2020-05-26 17:48:45 -0300

    Update Agenda

---
+++ Agenda/src/Telefone.java
@@ -0,0 +1,12 @@
+public class Telefone {
+    
+    private String numero;
+
+    public Telefone(String numero){
+        this.numero = numero;
+    }
+
+    public String getTelefone(){
+        return numero;
+    }
+}
